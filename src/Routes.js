5f13734270257c771f295737995b3b8b534e7baa
Phil Fernandez <norcal618@gmail.com>
2021-02-06 18:36:27 -0800

    kinda tracks logged in

--- src/Routes.js
+++ src/Routes.js
@@ -6,16 +6,17 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ToDoApp from "./ToDoApp";
 import history from "./history";
+import PrivateRoute from "./PrivateRoute";
 
 export default class Routes extends Component {
   render() {
     return (
       <Router history={history}>
         <Switch>
-          <Route path="/" exact component={ToDoApp} />
-          <Route path="/ListToDo" component={ListToDo} />
-          <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+          <PrivateRoute path="/" exact component={ToDoApp} />
+          <PrivateRoute path="/ListToDo" component={ListToDo} />
         </Switch>
       </Router>
     );
--- src/service/AuthService.js
+++ src/service/AuthService.js
@@ -3,26 +3,32 @@ import axios from "axios";
 const API_URL = "http://localhost:8080/";
 
 class AuthService {
-  async login(username, password) {
-    const response = await axios
-          .post(API_URL + "authenticate", {
-              username,
-              password,
-          });
-      if (response.data.token) {
-          localStorage.setItem("user", JSON.stringify(response.data));
-      }
-      return response.data;
-  }
-  logout() {
-    localStorage.removeItem("user");
-  }
-  register(username, password) {
-    return axios.post(API_URL + "register", {
-      username,
-      password,
-    });
-  }
+    constructor() {
+        this.loggedIn = false;
+    }
+    async login(username, password) {
+        const response = await axios.post(API_URL + "authenticate", {
+            username,
+            password,
+        });
+        if (response.data.token) {
+            this.loggedIn = true;
+            localStorage.setItem("user", JSON.stringify(response.data));
+        }
+        return response.data;
+    }
+    logout() {
+        localStorage.removeItem("user");
+    }
+    register(username, password) {
+        return axios.post(API_URL + "register", {
+            username,
+            password,
+        });
+    }
+    isLoggedIn() {
+        return this.loggedIn;
+    }
 }
 
 export default new AuthService();
